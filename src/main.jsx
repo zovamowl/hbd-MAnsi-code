852e50eb6223f52ee4bd0cb2201b8b8057c75480
zovamowl <hrishikeshberad9@gmail.com>
2025-06-24 23:46:37 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-// src/main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "../index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
